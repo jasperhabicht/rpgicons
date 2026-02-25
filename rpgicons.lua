bb3194318f76a876be2fbf7a8fdfb9795c3699bb
Jasper Habicht <mail@jasperhabicht.de>
2026-02-25 23:16:38 +0100

    Change lua to module

--- rpgicons.lua
+++ rpgicons.lua
@@ -11,44 +11,50 @@
 -- This work has the LPPL maintenance status `maintained'.
 --
 
+local open         = pdfe.open
+local pagestotable = pdfe.pagestotable
+local arraytotable = pdfe.arraytotable
+local mod          = math.mod
+local img_write    = img.write
+
 --- Stores the objnums of all pages of a PDF with the relevant page numbers.
 -- The objnums are stored as keys of the table.
 -- The page numbers are stored as values to the relevant keys. 
-local rpgicons_pages = {}
+local _pages = {}
 
 --- Stores the named dests in a PDF with the relevant page numbers.
 -- The named dests are stored as keys of the table.
 -- The page numbers are stored as values to the relevant keys. 
-local rpgicons_dests = {}
+local _dests = {}
 
 --- Retrieves the page numbers of all named dests in a PDF.
 -- Stores the objnums of all pages of the PDF file in rpgicons_pages (table). 
 -- For each named dest, gets the objnum of the referenced page
 -- and stores the relevant page number in rpgicons_dests (table).
 -- @param file The PDF file to be processed.
-function rpgicons_get_dest_pages(file)
-    local rpgicons_file = pdfe.open(file)
-    for i, o in pairs(pdfe.pagestotable(rpgicons_file)) do
+function get_dest_pages(file)
+    local _file = open(file)
+    for i, o in pairs(pagestotable(_file)) do
         -- The third item of the array obtained by pdfe.arraytotable
         -- contains the objnum.
-        rpgicons_pages[o[3]] = i
+        _pages[o[3]] = i
     end
 
-    local rpgicons_dest_current = ''
-    local rpgicons_dest_names = rpgicons_file.Catalog.Names.Dests.Names
-    for i, o in pairs(pdfe.arraytotable(rpgicons_dest_names)) do
+    local _dest_current = ''
+    local _dest_names = _file.Catalog.Names.Dests.Names
+    for i, o in pairs(arraytotable(_dest_names)) do
         -- The PDF item /Dests/Names contains pairs
         -- consisting of dest name and object reference.
-        if (math.mod(i, 2) ~= 0) then
-            rpgicons_dest_current = rpgicons_dest_names[i]
+        if (mod(i, 2) ~= 0) then
+            _dest_current = _dest_names[i]
         else
             -- The PDF item /D contains a single pair
             -- consisting of object reference and fit parameter.
             -- The third item of the array obtained by pdfe.arraytotable
             -- contains the objnum.
-            rpgicons_dests[rpgicons_dest_current] =
-                rpgicons_pages[
-                    pdfe.arraytotable(rpgicons_dest_names[i].D)[1][3]
+            _dests[_dest_current] =
+                _pages[
+                    arraytotable(_dest_names[i].D)[1][3]
                 ]
         end
     end
@@ -60,8 +66,15 @@ end
 -- @param dest The named dest.
 -- @param file The PDF file.
 -- @return PDF page as TeX node.
-function rpgicons_load_icon(dest, file)
-    return img.write({page = rpgicons_dests[dest], filename = file})
+function load_icon(dest, file)
+    return img_write({page = _dests[dest], filename = file})
 end
+
+rpgicons = {
+    get_dest_pages = get_dest_pages,
+    load_icon      = load_icon
+}
+
+return rpgicons
 
 -- EOF
